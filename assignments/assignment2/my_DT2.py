80a89723be0a8be4bc47e0c07d26c7b8a314402e
rj08-97 <53830878+rj08-97@users.noreply.github.com>
2020-09-03 23:13:12 -0400

    my_dt

---
+++ assignments/assignment2/my_DT2.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+
+class my_DT:
+
+    def __init__(self, criterion="gini", max_depth=8, min_impurity_decrease=0, min_samples_split=2):
+        # criterion = {"gini", "entropy"},
+        # Stop training if depth = max_depth
+        # Only split node if impurity decrease >= min_impurity_decrease after the split
+        #   Weighted impurity decrease: N_t / N * (impurity - N_t_R / N_t * right_impurity - N_t_L / N_t * left_impurity)
+        # Only split node with >= min_samples_split samples
+        self.criterion = criterion
+        self.max_depth = int(max_depth)
+        self.min_impurity_decrease = min_impurity_decrease
+        self.min_samples_split = int(min_samples_split)
+
+    def impurity(self, labels):
+        # Calculate impurity (unweighted)
+        # Input is a list (or np.array) of labels
+        # Output impurity score
+        stats = Counter(labels)
+        N = float(len(labels))
+        impure = 0.0
+        if self.criterion == "gini":
+            for i in stats:
+                length = len(i)
+                score = 0.0
+                p = stats.count(i) / (N)
+                score += p * p
+            impure += (1.0 - score) * (length / N)
+        elif self.criterion == "entropy":
+            for i in range(len(stats)):
+                impure += np.sum([(-stats[i] / np.sum(N)) * np.log2(stats[i] / np.sum(N))])
+        else:
+            raise Exception("Unknown criterion.")
+        return impure
+
+    def find_best_split(self, pop, X, labels):
+        # Find the best split
+        # Inputs:
+        #   pop:    indices of data in the node
+        #   X:      independent variables of training data
+        #   labels: dependent variables of training data
+        # Output: tuple(best feature to split, weighted impurity score of best split, splitting point of the feature, [indices of data in left node, indices of data in right node], [weighted impurity score of left node, weighted impurity score of right node])
+        ######################
+        best_feature = None
+        for feature in X.keys():
+            cans = np.array(X[feature][pop])
+
+        return best_feature
+
+    def fit(self, X, y):
+        # X: pd.DataFrame, independent variables, float
+        # y: list, np.array or pd.Series, dependent variables, int or str
+        self.classes_ = list(set(list(y)))
+        # write your code below
+        # X: pd.DataFrame, independent variables, float
+        # y: list, np.array or pd.Series, dependent variables, int or str
+
+        return
+
+    def predict(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # return predictions: list
+        # write your code below
+        return predictions
+
+    def predict_proba(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # Eample:
+        # self.classes_ = {"2", "1"}
+        # the reached node for the test data point has {"1":2, "2":1}
+        # then the prob for that data point is {"2": 1/3, "1": 2/3}
+        # return probs = pd.DataFrame(list of prob, columns = self.classes_)
+        # write your code below
+
+        ##################
+        return probs
