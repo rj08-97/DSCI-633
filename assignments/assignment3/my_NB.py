14b659370706ea7aede6e7a4f2928e480cbc4de5
rakshandajha <rxj7063@rit.edu>
2020-09-11 19:50:36 -0400

    Finished

--- assignments/assignment3/my_NB.py
+++ assignments/assignment3/my_NB.py
@@ -3,8 +3,6 @@ import numpy as np
 from collections import Counter
 
 
-
-
 class my_NB:
 
     def __init__(self, alpha=1):
@@ -17,62 +15,40 @@ class my_NB:
     def fit(self, X, y):
         # X: pd.DataFrame, independent variables, str
         # y: list, np.array or pd.Series, dependent variables, int or str
+
+        # instantiate self.classes_
         self.classes_ = list(set(list(y)))
+        # instantiate self.P_y
         self.P_y = Counter(y)
 
-        self.P = {}
-        #stores the key values of P_y
-        y_val = list(self.P_y.keys())
-        #intializes the list for label,key and feature/value storing
-        val_counts_list = []
-        prob_val_list = []
-        val_arr_list = []
-        key_val_list = []
+        features = [feature for feature in X]
+        feature_values = [X[feature].unique() for feature in features]
+        feature_probabilities = dict()
+        labels = [label for label in self.classes_]
 
-        for i in range(len(y_val)):
-            print("i", i)
-            for j in range(len(X.columns)):
-                val_count = []
-                val_arr = []
-                h = [k for k in range(0, len(X[j]))]
-                key_val = [X[j][k] for k in h]
-                for k in h:
-                    try:
-                        val_count.append(X[j][y.loc[y == y_val[i]].index.values].value_counts()[X[j][k]] + self.alpha)
-                        val_arr.append(X[j][y.loc[y == y_val[i]].index.values].value_counts()[X[j][k]])
-                    except:
-                        val_arr.append(0)
-                        val_count.append(0 + self.alpha)
-                num_avail_cat = len(X[j])
-                val_arr_list.append(val_arr)
-                val_counts_list.append(val_count)
-                prob_val_list.append(val_count / (sum(val_arr) + num_avail_cat * self.alpha))
-                key_val_list.append(key_val)
+        for label in labels:
+            number_of_occurences_of_label = Counter(y)[label]
+            feature_attribute_probability_value = dict()
+            for feature in range(0, len(feature_values)):
+                # number of rows
+                all_possible_feature_values = feature_values[feature]
+                feature_attribute_probability_value[feature] = dict()
+                number_of_each_possible_value = Counter(X[feature])
 
-        for n in range(0, len(y_val)):
-            dict_a = {}
-            dict_b = {}
-            for column in range(0, len(X.columns)):
-                for k in range(0,len(key_val_list[j])):
-                    dict_b[key_val_list[j][k]] = prob_val_list[j][k]
-                dict_a[column] = dict_b
-            self.P[y_val[n]] = dict_a
+                for feat_val in all_possible_feature_values:
+                    feature_attribute_probability_value[feature][feat_val] = dict()
+                    feat_prob = (sum(
+                                        [feat_val == X[feature][entry] and y[entry] == label for entry in range(len(X[feature]))]
+                                    ) + self.alpha
+                                ) / ( number_of_occurences_of_label +
+                                    (len(number_of_each_possible_value.keys()) * self.alpha)
+                                )
 
+                    feature_attribute_probability_value[feature][feat_val] = feat_prob
 
-        total_labels = len(list(y))
-        counted_labels = dict()
-        for label in list(y):
-            if label in counted_labels:
-               counted_labels[label] = counted_labels[label] + 1
-            else:
-                counted_labels[label] = 1
+            feature_probabilities[label] = feature_attribute_probability_value
 
-        prob_y = dict()
-        for key in counted_labels:
-            prob_y[key] = counted_labels[key]/total_labels
-        self.P_y = prob_y
-
-        return
+        self.P = feature_probabilities
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables, str
