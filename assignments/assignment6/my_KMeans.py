6fb418d7fd71d6223cd7acf9fe9d7872546823a3
rakshandajha <rxj7063@rit.edu>
2020-10-03 16:57:02 -0400

    Finished

--- assignments/assignment6/my_KMeans.py
+++ assignments/assignment6/my_KMeans.py
@@ -6,7 +6,7 @@ import math
 
 class my_KMeans:
 
-    def __init__(self, n_clusters=8, init="random", n_init=10, max_iter=300, tol=1e-4):
+    def __init__(self, n_clusters=8, init="k-means++", n_init=10, max_iter=300, tol=1e-4):
         # init = {"k-means++", "random"}
         # stop when either # iteration is greater than max_iter or the delta of self.inertia_ is smaller than tol.
         # repeat n_init times and keep the best run (cluster_centers_, inertia_) with the lowest inertia_.
