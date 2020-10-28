2c9e74f97e0f58b70acd48615d208ba07e7d0cad
rakshandajha <rxj7063@rit.edu>
2020-10-27 23:27:52 -0400

    assignment 9

--- assignments/assignment9/my_GA.py
+++ assignments/assignment9/my_GA.py
@@ -74,7 +74,7 @@ class my_GA:
             for fold in range(self.crossval_fold):
                 start = int(fold * size)
                 end = start + size
-                test_indices = indices[start:end]
+                test_indices = indices[start:]
                 train_indices = indices[:start] + indices[:end]
                 X_train = self.data_X.loc[train_indices]
                 X_train.index = range(len(X_train))
@@ -90,9 +90,9 @@ class my_GA:
                 actuals = np.array(y_test)
                 objs = np.array(self.obj_func(predictions, actuals, pred_proba))
                 if type(objs_crossval) == type(None):
-                    objs_crossval = 1
+                    objs_crossval = objs[fold]
                 else:
-                    objs_crossval += 1
+                    objs_crossval += objs
 
             objs_crossval = objs_crossval / float(len(self.data_y))
             self.evaluated[decision] = objs_crossval
@@ -108,7 +108,7 @@ class my_GA:
         obj_a = self.evaluate(a)
         obj_b = self.evaluate(b)
         # write your own code below
-        if obj_a > obj_a:
+        if a > b:
             return 1
         else:
             return -1
@@ -157,7 +157,7 @@ class my_GA:
         # self.generation = survived points
 
         # single-objective:
-        if self.evaluate(self.generation[0]) == 1:
+        if len(self.evaluate(self.generation[0])) == 1:
             selected = np.argsort([self.evaluate(x)[0] for x in self.generation])[::-1][
                        :int(np.ceil(self.selection_rate * self.generation_size))]
             self.pf = [self.generation[selected[0]]]
@@ -190,7 +190,7 @@ class my_GA:
         def cross(a, b):
             new_point = []
             for i in range(len(a)):
-                if np.random.choice([a,b]) == a:
+                if np.random.choice(a) == a:
                     new_point.append(a[i])
                 else:
                     new_point.append(b[i])
