6107a29df0cf58d05ab46c2d6c0f09a29381eb58
rakshandajha <rxj7063@rit.edu>
2020-11-19 04:07:16 -0500

    Project

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -4,33 +4,27 @@ import time
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import PassiveAggressiveClassifier
 
-from sklearn.tree import DecisionTreeClassifier
-
 from assignment8.my_evaluation import my_evaluation
-
 
 
 class my_model():
     def __init__(self):
         self.preprocessor = CountVectorizer(stop_words='english')
-        self.clf = PassiveAggressiveClassifier(C=0.1, fit_intercept=True)
+        self.clf = PassiveAggressiveClassifier(C=0.1, fit_intercept=True, n_iter_no_change=20, validation_fraction=0.8)
 
     def fit(self, X, y):
         # do not exceed 29 mins
         XX = self.preprocessor.fit_transform(X["description"])
-        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False,sublinear_tf=True).transform(XX)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).transform(XX)
         self.clf.fit(X_final, y)
         return
 
     def predict(self, X):
         # remember to apply the same preprocessing in fit() on test data before making predictions
         XX = self.preprocessor.transform(X["description"])
-        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False,sublinear_tf=True).transform(XX)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).transform(XX)
         predictionsOfModel = self.clf.predict(X_final)
         return predictionsOfModel
-    # change the name to predictions before submission
-
-
 
 
 if __name__ == "__main__":
