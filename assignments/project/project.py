bee9e88accaaf1c5483384081ba9693a27725875
rakshandajha <rxj7063@rit.edu>
2020-11-19 01:49:46 -0500

    Project

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import time
 
-from sklearn import preprocessing
-from sklearn.decomposition import PCA
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import PassiveAggressiveClassifier
 
 from sklearn.tree import DecisionTreeClassifier
@@ -11,23 +9,24 @@ from sklearn.tree import DecisionTreeClassifier
 from assignment8.my_evaluation import my_evaluation
 
 
+
 class my_model():
     def __init__(self):
-        self.preprocessor = TfidfVectorizer(stop_words='english', norm='l2', use_idf=False, smooth_idf=False)
-        self.clf = PassiveAggressiveClassifier()
+        self.preprocessor = CountVectorizer(stop_words='english')
+        self.clf = PassiveAggressiveClassifier(C=0.1, fit_intercept=True)
 
     def fit(self, X, y):
         # do not exceed 29 mins
         XX = self.preprocessor.fit_transform(X["description"])
-        self.clf.get_params(deep=True)
-        self.clf.fit(XX, y)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False,sublinear_tf=True).transform(XX)
+        self.clf.fit(X_final, y)
         return
 
     def predict(self, X):
         # remember to apply the same preprocessing in fit() on test data before making predictions
-        transform = PCA(n_components=100)
         XX = self.preprocessor.transform(X["description"])
-        predictionsOfModel = self.clf.predict(XX)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False,sublinear_tf=True).transform(XX)
+        predictionsOfModel = self.clf.predict(X_final)
         return predictionsOfModel
     # change the name to predictions before submission
 
