469a2a50acc3196cd1fceac19bd036f4f7c388ea
rakshandajha <rxj7063@rit.edu>
2020-11-21 11:40:48 -0500

    Project

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -5,26 +5,39 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import PassiveAggressiveClassifier
 
 
-class my_model():
+def cleanup_feature(feat):
+    return feat.map(lambda x: str(x).strip("#" + " "))
+
+
+def get_train_test_df(X):
+    return cleanup_feature(X["title"]) + " " \
+           + cleanup_feature(X["description"]) + " " \
+           + cleanup_feature(X["telecommuting"]) + " " \
+           + cleanup_feature(X["has_questions"])
+
+
+class my_model:
     def __init__(self):
+        # defines the self function used in fit and predict
         self.preprocessor = CountVectorizer(stop_words='english')
-        self.clf = PassiveAggressiveClassifier(C=1, fit_intercept=True, n_iter_no_change=20, validation_fraction=0.8)
+        self.clf = PassiveAggressiveClassifier(C=0.1, fit_intercept=True, n_iter_no_change=10, validation_fraction=0.8)
 
     def fit(self, X, y):
         # do not exceed 29 mins
-        X_df = X["title"] + " " + X["description"]
+        X_df = get_train_test_df(X)
         XX = self.preprocessor.fit_transform(X_df)
-        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).transform(XX)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).fit_transform(XX)
         self.clf.fit(X_final, y)
         return
 
     def predict(self, X):
         # remember to apply the same preprocessing in fit() on test data before making predictions
-        X_df = X["title"] + " " + X["description"]
+        X_df = get_train_test_df(X)
         XX = self.preprocessor.transform(X_df)
-        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).transform(XX)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).fit_transform(XX)
         predictionsOfModel = self.clf.predict(X_final)
         return predictionsOfModel
+
 
 if __name__ == "__main__":
     start = time.time()
